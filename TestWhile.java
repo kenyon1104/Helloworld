8e66011508f944be1ce8e2809fef31e11b80eda1
DESKTOP-GUU183L\T460 <kenyon1104@gmail.com>
2023-02-16 11:56:42 +0800

    test 循环结构

---
+++ TestWhile.java
@@ -0,0 +1,10 @@
+ public class TestWhile{
+	 public static void main(String[] args) {
+		 int a =10;
+		 while(a < 20) {
+			 System.out.println("a的值是" + a);
+			 a++;
+			 System.out.println("\n");
+		 }
+	 }
+ }
