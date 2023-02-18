2b456bcc4261e43ab3d3767028c68a934e8507bf
DESKTOP-GUU183L\T460 <kenyon1104@gmail.com>
2023-02-18 21:34:22 +0800

    codecademy test1

---
+++ TimeLine.java
@@ -0,0 +1,16 @@
+public class TimeLine {
+  public static void main(String[] args) {
+    System.out.println("Hello Java!");
+   
+    System.out.println("You were born in 1995");
+
+    //Sun Microsystems announced the release of Java in 1995
+    
+    System.out.println("You were created by James Gosling");
+    
+	/*James is funny
+    */
+    
+    System.out.println("You are a fun language!");
+  }
+} 
