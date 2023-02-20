a15a8caa95d52476f0a1d0c63a465acd2675e558
DESKTOP-GUU183L\T460 <kenyon1104@gmail.com>
2023-02-20 21:29:46 +0800

    codecademy learn2

---
+++ Boolean.java
@@ -0,0 +1,6 @@
+public class Boolean {
+	public static void main(String[] args) {    
+    boolean intsCanHoldDecimals = false;
+   System.out.println(intsCanHoldDecimals);
+	}
+}
---
+++ CountComment.java
@@ -0,0 +1,11 @@
+public class CountComment {
+
+	public static void main(String[] args) {
+ 
+    int numComments;
+  
+    numComments = 6;
+    System.out.println(numComments);
+	}
+}
+
