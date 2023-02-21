65b96d7d533336a904a873a4551495aee6a4a08d
DESKTOP-GUU183L\T460 <kenyon1104@gmail.com>
2023-02-21 21:17:48 +0800

    韩 变量入门

---
+++ Var01.java
@@ -0,0 +1,13 @@
+public class Var01 {
+	//编写一个main方法
+	public static void main(String[] args) {
+		//声明变量
+		int a;
+		a = 100;
+		System.out.println(a);
+		
+		//也可以
+		int b = 800;
+		System.out.println(b);
+	}
+}
